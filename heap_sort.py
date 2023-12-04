051936c84156b323fa5f0560fac4bae7373984ce
rachyoung <youngrachel@ufl.edu>
2023-12-04 06:15:42 -0500

    comments for heap_sort.py and main.py

--- heap_sort.py
+++ heap_sort.py
@@ -1,7 +1,8 @@
-import heapq
+import heapq  # module performs min heap operations on a list
 
 
 def heap_sort(data, sort_parameter) -> set:
+    # dictionary to pair attribute names with their indices in a song entry
     param_dict = {"title":  0,
                   "rank":   1,
                   "date":   2,
@@ -12,26 +13,28 @@ def heap_sort(data, sort_parameter) -> set:
                   "trend":  7,
                   "stream": 8}
 
-    # move sort_parameter to index 0 for heap building purposes
+    # swap sort_parameter with index 0 for heap building purposes
     for entry in data:
         entry[param_dict[sort_parameter]], entry[0] = entry[0], entry[param_dict[sort_parameter]]
     # update dictionary to reflect swap
     param_dict[sort_parameter], param_dict["title"] = param_dict["title"], param_dict[sort_parameter]
 
-    heapq.heapify(data)  # build min heap
+    # build min heap
+    heapq.heapify(data)
     sorted_songs = []  # stores the sorted songs as they're extracted from the heap
+    # load songs into sorted_songs list
     while True:
         try:
-            song_entry = heapq.heappop(data)
+            song_entry = heapq.heappop(data)  # extract_min() from min heap
+            # add song title and spotify url into sorted_songs
             sorted_songs.append((song_entry[param_dict["title"]],
                                  song_entry[param_dict["url"]]))
-        except IndexError:
+        except IndexError:  # reached end of the data
             break
 
-    ten_songs = set()
-
+    ten_songs = set()  # stores the 10 songs selected from sorted_songs for the quiz (no duplicates)
     i = 0
-    while len(ten_songs) < 10:
+    while len(ten_songs) < 10:  # keep adding songs until the set contains 10 songs
         ten_songs.add(sorted_songs[i])
         i += 1
 
--- main.py
+++ main.py
@@ -7,52 +7,59 @@ import glob
 import subprocess
 
 
+# function uses the spotdl library to download an mp3 file given a track's spotify url
 def download_mp3_with_spotdl(song_url):
     try:
-        # using subprocess to run the spotdl command to download the song
+        # use subprocess to run spotdl CLI to download the mp3 file
         subprocess.run(["spotdl", song_url])
         return True
     except subprocess.CalledProcessError:  # download error
         return False
 
 
+# main function called by app.py to parse the CSV file, compare sorts, and select 10 songs for the quiz
 def main(region, sort_metric):
-    param_dict = {"title": 0,
-                  "rank": 1,
-                  "date": 2,
+    # dictionary to pair attribute names with their indices in a song entry
+    param_dict = {"title":  0,
+                  "rank":   1,
+                  "date":   2,
                   "artist": 3,
-                  "url": 4,
+                  "url":    4,
                   "region": 5,
-                  "chart": 6,
-                  "trend": 7,
+                  "chart":  6,
+                  "trend":  7,
                   "stream": 8}
 
+    # read first 100,000 entries in the CSV file
     count = 0
-    songs = []
+    songs = []  # stores the CSV file entries
+    # open the CSV file
     with open('charts.csv', newline='') as song_list:
         song_reader = csv.reader(song_list, delimiter=',')
         for row in song_reader:
             if count > 0 and row[param_dict["region"]] == region:  # ignore the row with attribute titles
                 songs.append(row)
             count += 1
-            if count > 100000:  # arbitrary stopping point in dataset traversal, >100,000 datapoints
+            if count > 100000:  # stop reading after 100,000 entries
                 break
 
+    # time heap sort
     start_heap = timer()
     ten_songs = heap_sort(songs, sort_metric)
     end_heap = timer()
     heap_time = (end_heap - start_heap)
 
+    # time merge sort
     start_merge = timer()
     merge_sort(songs, sort_metric)
     end_merge = timer()
     merge_time = (end_merge - start_merge)
 
     ten_files = []
-    print(len(ten_songs))
+    # heap and merge sort should return same results, use heap sort result to download mp3 files
     for entry in ten_songs:
-        download_mp3_with_spotdl(entry[1])
-        print(entry[1])
+        download_mp3_with_spotdl(entry[1])  # download mp3 using spotify url
+        # change file names and move to static/mp3 directory
         list_of_files = glob.glob("/Users/rachelyoung/PycharmProjects/COP3530Project3/DSA-Dumplings-Project-3/*")
         latest_file = max(list_of_files, key=os.path.getctime)
         splice_index = latest_file.find("-3/") + 3
@@ -61,9 +68,11 @@ def main(region, sort_metric):
                   f"/Users/rachelyoung/PycharmProjects/COP3530Project3/DSA-Dumplings-Project-3/static/mp3/{file_name}")
         ten_files.append((entry[0], file_name))
 
+    # add merge and heap sort run times to the list to be returned
     ten_files.append((f'Merge Sort Run-time: {merge_time} s', f'Heap Sort Run-time: {heap_time} s'))
     for file in ten_files:
         print(' || '.join(file))
+
     return ten_files
 
 
