f41da03c7a24f6efcf4b978cfe424f2a3f35308f
Dehpeh <dhpark602@gmail.com>
2023-12-04 03:57:15 -0500

    update merge sort for spotify/csv

--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,67 @@
-def merge_sort(arr):
+def merge_sort(data, sort_parameter) -> set:
+    # Dictionary to map sort parameters to their corresponding indices
+    param_dict = {"title": 0,
+                  "rank": 1,
+                  "date": 2,
+                  "artist": 3,
+                  "url": 4,
+                  "region": 5,
+                  "chart": 6,
+                  "trend": 7,
+                  "stream": 8}
+
+    # if data has one element or is empty, it is already sorted
+    if len(data) <= 1:
+        return {tuple(entry["title"], entry["artist"], entry["url"]) for entry in data}
+
+    # stack for iterative recursion
+    stack = [(0, len(data))]
+    sorted_tuples = set()
+
+    while stack:
+        start, end = stack.pop()
+        size = end - start
+
+        # if subarray has 1 element, add it to result
+        if size <= 1:
+            sorted_tuples.add(tuple(data[start]["title"], data[start]["artist"], data[start]["url"]))
+        else:
+            mid = start + size // 2
+            stack.append((mid, end))
+            stack.append((start, mid))
+
+            # extract left and right halves
+            left = data[start:mid]
+            right = data[mid:end]
+            merged = []
+            i = j = 0
+
+            # merge the two halves
+            while i < len(left) and j < len(right):
+                if left[i][param_dict[sort_parameter]] < right[j][param_dict[sort_parameter]]:
+                    merged.append(left[i])
+                    i += 1
+                else:
+                    merged.append(right[j])
+                    j += 1
+
+            # add any elements left
+            merged.extend(left[i:])
+            merged.extend(right[j:])
+
+            # update the original data with the merged result
+            data[start:end] = merged
+
+    # select the top 10 songs
+    ten_songs = set()
+    i = 0
+    while len(ten_songs) < 10 and i < len(sorted_tuples):
+        ten_songs.add(sorted_tuples[i])
+        i += 1
+
+    return ten_songs
+
+def template_merge_sort(arr):
     if len(arr) > 1:
         mid = len(arr) // 2  # Find the middle of the list
         left_half = arr[:mid]  # Divide the list into two halves
@@ -28,9 +91,3 @@ def merge_sort(arr):
             arr[k] = right_half[j]
             j += 1
             k += 1
-
-
-# Template main >:)
-my_list = [38, 27, 43, 3, 9, 82, 10]
-merge_sort(my_list)
-print("Sorted List:", my_list)
